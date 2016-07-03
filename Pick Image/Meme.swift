077a8035d892be7f7db3be9a2c5b32d54355c1d5
Isaac To <isaac.chun.to@gmail.com>
2016-07-02 21:39:02 -0500

    Add Meme structure

---
+++ Pick Image/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  Pick Image
+//
+//  Created by Isaac To on 7/2/16.
+//  Copyright © 2016 Isaac To. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    let topText: String
+    let bottomText: String
+    let originalImage: UIImage
+    let memeImage: UIImage
+}
